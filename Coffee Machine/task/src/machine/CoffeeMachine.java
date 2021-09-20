6a2e76cde9c5b5d5113d105102bd4224e3fd507b
amamic <antonia.mamic1@gmail.com>
2021-09-20 21:26:01 +0200

    2/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,4 +1,5 @@
 package machine;
+import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
@@ -10,7 +11,17 @@ public class CoffeeMachine {
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
 
+        int water = 200, milk = 50, beans = 15;
 
+        Scanner s = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need:");
+        int numberOfCups = s.nextInt();
+
+
+        System.out.println("For 25 cups of coffee you will need:\n" +
+                numberOfCups * water + " ml of water\n" +
+                numberOfCups * milk + " ml of milk\n" +
+                numberOfCups * beans + " g of coffee beans");
 
     }
 }
